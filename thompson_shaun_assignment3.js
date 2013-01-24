6fbe08bc6114e4fbd65fa81996e70e4700465b4c
Shaun Thompson <shaunthompson@fullsail.edu>
2013-01-24 16:19:10 -0500

    Coding layout for story telling completed

--- thompson_shaun_assignment3.js
+++ thompson_shaun_assignment3.js
@@ -1,1 +1,46 @@
-alert("JavaScript works!");
+// Shaun M Thompson
+// SDI 1301
+// 1/24/2013
+// Assignment 3
+
+//---START----
+
+	//Object
+	//property boolean 		-- employee is a teck/yes or no
+	//property array 		-- phones employee can work on
+	//property number		-- hours employee is working
+	//property string 		-- names of phone employee can work on
+	//property object 		-- ??
+	
+	//method procedure 		-- NO RETURN {all else requires return}
+	
+	//method function		--
+	//method accessor		--
+	//method mutator		--
+
+//Argument Boolean
+	//back
+	//math
+	//return boolean
+	
+//Argument String
+	//local variables
+	//return string
+
+//Argument Number
+	//for loop
+		//nested loop
+	//return number
+	
+//Argument Array
+	//while loop
+	//return array
+
+
+//---MAIN CODE---
+	
+//Output
+//Retuned values
+
+
+//---FINISH----
