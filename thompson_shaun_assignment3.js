6fe44dcd1924104c5996203b2349775e38647e1d
Shaun Thompson <shaunthompson@fullsail.edu>
2013-01-24 16:38:48 -0500

    Object layout put together and code structure written.

--- thompson_shaun_assignment3.js
+++ thompson_shaun_assignment3.js
@@ -17,6 +17,50 @@
 	//method function		--
 	//method accessor		--
 	//method mutator		--
+	
+	var employee = { 
+	name: "Jon",																//property string
+	technician: true;															//property boolean
+	experience: 3,																//property number
+	strengths: ["Repairs", "Sales", "Time Management"],							//property array
+	greetCust: function() {														//method procedure
+		console.log("Hello, my name is " + name + ", How may I help you?");
+	},
+	totalDevices: function() {													//method accessor							
+		//return
+	},
+	changeDevices: function(){
+		//return
+	}
+};
+
+var key = "strengths";
+
+console.log(student[key]);
+console.log(student.strengths);
+console.log(student["strengths"]);
+
+console.log(student["greetCust"]);
+console.log(student.greetCust);
+
+employee.greetCust();
+employee["greetCust"]();
+
+employee.greetCust = function() {
+	console.log("I was fired today, I'm outta here!");
+	};
+	
+employee.greetCust();
+
+//how do we access all of this information?
+
+for (var key in employee) {
+	console.log("Key: " + key + ", Value: ", employee[key]);
+};
+
+// KEY variable is overwritten each time the loop runs.
+//student.key = key is a value in the object
+//student[key] = i am calling the variable key
 
 //Argument Boolean
 	//back
@@ -44,3 +88,5 @@
 
 
 //---FINISH----
+
+	
