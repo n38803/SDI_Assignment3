463d2af53d9fefd52ac66dca0db9d14d9c756dc2
Shaun Thompson <shaunthompson@fullsail.edu>
2013-01-26 16:34:33 -0500

    Getting oh so close

--- thompson_shaun_assignment3.js
+++ thompson_shaun_assignment3.js
@@ -3,8 +3,8 @@
 // 1/24/2013
 // Assignment 3
 
-//---START----
 
+//----GUIDE--------------------------------------------------------------------------------------
 	//Object				-- phone
 	//property boolean 		-- the phone is repairable
 	//property array 		-- types of known issues for the phone
@@ -18,54 +18,63 @@
 	//method accessor		--
 	//method mutator		--
 
-//--GLOBAL VARIABLES	
-var repairTime = 10; 
-var empNames = ["Shaun", "Jon", "Eric", "Matt, Joseph, Danny, Thomas, Ralf, Jacob, Henry"];
-var adjectives = [
+
+
+//----START--------------------------------------------------------------------------------------
+
+//----GLOBAL VARIABLES----	
+var totalCustomers = 3,
+	totalRepairs = 5,
+	downloadedApps = 20,
+	empNames = ["Shaun", "Jon", "Eric", "Matt, Joseph, Danny, Thomas, Ralf, Jacob, Henry"],
+	adjectives = [
 	"sleeps on the job",
 	"doesn't even know where he is",
 	"calls out all the time.",
 	"are all genuinely crazy."
 	]; 
-//--------------------------------------------------
+//------------------------
 	
 
 
-//--OBJECT
+//----MAIN OBJECT---------
 var store = {
 	name: "Wireless South Jersey",										//property string
 	staff: 12,															//property number
 	technicians: 6,														//property number
+			
 	termination: function(exhausted) {
-		console.log("After further deliberation, I've decided to terminate everyone!");
-		for (var i=0, j=empNames.length; i < j; i++) {
+		console.log("Unfortunately, none of my technicians were able to complete your repair.");
+		for (var i=0, j=empNames.length; i < j; i++) {					//for loop
 			console.log(empNames[i] + " " + adjectives[i] + " ...");
 		}; 
+		var fired = "Leave your tools and get out!";
 		console.log(exhausted);
+		return fired;													//return string
 	},								//method procedure
 		
 	repair: device = { 
 		name: "HTC EVO",												//property string
 		repairable: true,												//property boolean
 		problems: 6,													//property number
-		issues: ["Hardware", "Software", "User"],						//property array
+		issues: ["No Volume", "Freezing", "Drains Battery"],			//property array
 	
 		software: function(appAmnt, totalIssues) {									
 		if (appAmnt > 0) {
-			console.log("By golly, we have some work to do!");
+			console.log("There are some minor things we will need to go over before beginning the repair though.");
+			
 		} else {
-			console.log("I guess there is no software issue with this..")
+			console.log("We were unable to locate any issues with the device.")
 		};
 		var outcome = appAmnt * totalIssues;
-			console.log("There are " + outcome + " possible directories for the root of your issue(s).");
-			return outcome;
+			return outcome;												//number return
 	},					//method function
 	
 		repairTime: function() {
 		var totalTime = this.problems * this.issues.length / store.technicians;
 		return totalTime;
 	},									//method accessor (getter)
-	
+
 		addProblem: function(newProblem){
 		this.problems = newProblem;
 		}							//method mutator (changer)											
@@ -73,22 +82,27 @@ var store = {
 	
 	
 };
+//------------------------
+	
 
-//----Main CODE-----
-
-	//call procedure
-device.software(9, device.problems);
-
+//----MAIN CODE-----------
+	
 	//call accessor
-device.repairTime();
-	//output accessor function return
-console.log("The time it will take to repair this is generally: " + device.repairTime() + " hours.");
+	var totalTime = device.repairTime();
+	console.log("Based off of the initial description provided, the repair itself will take no less than " + totalTime + " hours.");
+
+	//number argument + object argument
+	var posIssues = device.software(totalRepairs, device.problems);
+	console.log("Based off of the amount of downloaded applications (" + downloadedApps + "), the amount of variable directories for your issue amounts to nearly " + posIssues + ".");
+
 
 	//call mutator
-device.addProblem(20);
-	//output mutator function return
-console.log("But if you are saying there are more issues, it's going to take " + device.repairTime() + " hours to repair your " + device.name + ".");
+	device.addProblem(downloadedApps);												
+	console.log("Therefore, we advise up to " + device.repairTime() + " hours total to diagnose and repair your " + device.name + ".");
 
+	//string argument
+	var fired = store.termination("... I just can't take it anymore!");
+	console.log(fired);
+//------------------------
 
-store.termination("... I just can't take it anymore!");
-
+//----FINISH-----------------------------------------------------------------------------------------
