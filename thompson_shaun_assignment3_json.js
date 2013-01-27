51438b5b096a026f500e5b50eef5f956d0aad1b0
Shaun Thompson <shaunthompson@fullsail.edu>
2013-01-26 19:34:32 -0500

    Trying to get my json file up

---
+++ thompson_shaun_assignment3_json.js
@@ -0,0 +1,46 @@
+// Shaun M Thompson
+// SDI 1301
+// 1/24/2013
+// Assignment 3 - Control File
+
+
+var json = {
+	"employees" : [
+		{
+			"id": "001",
+			"name": "Shaun",
+			"age": 28,
+			"status": "Terminated"
+		},
+	{
+			"id": "002",
+			"name": "Jon",
+			"age": 25,
+			"status": "Terminated"
+		},
+	{
+			"id": "003",
+			"name": "Eric",
+			"age": 21,
+			"status": "Terminated"
+		},
+	{
+			"id": "004",
+			"name": "Matt",
+			"age": 19,
+			"status": "Terminated"
+		},
+	{
+			"id": "005",
+			"name": "Joseph",
+			"age": 24,
+			"status": "Terminated"
+		},
+	{
+			"id": "006",
+			"name": "Henry",
+			"age": 23,
+			"status": "Terminated"
+		},
+	]
+};
